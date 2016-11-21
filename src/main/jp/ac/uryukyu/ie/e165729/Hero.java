544add27bdd0dbd3ed1e1ade8463266a28af8b5f
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2016-11-21 13:05:47 +0900

    First file

---
+++ src/main/jp/ac/uryukyu/ie/e165729/Hero.java
@@ -0,0 +1,19 @@
+package jp.ac.uryukyu.ie.e165729;
+
+/**
+ * ヒーロークラス。
+ *  String name; //敵の名前
+ *  int hitPoint; //敵のHP
+ *  int attack; //敵の攻撃力
+ *  boolean dead; //敵の生死状態。true=死亡。
+ * Created by tnal on 2016/11/13.
+ */
+
+public class Hero extends LivingThing {
+
+    public  Hero (String name, int maximumHP, int attack){
+        super(name, maximumHP, attack);
+        //System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+
+    }
+}
---
+++ src/main/jp/ac/uryukyu/ie/e165729/LivingThing.java
@@ -0,0 +1,44 @@
+package jp.ac.uryukyu.ie.e165729;
+
+/**
+ * Created by e165729 on 2016/11/20.
+ */
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing(String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void attack(LivingThing opponent) {
+        if (dead == false) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint < 0) {
+            dead = true;
+            System.out.printf("%sは道半ばで力尽きてしまった。\n", name);
+        }
+    }
+}
