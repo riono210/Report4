20c54f288ebc1eacda82325ea113fc4d7ef0b863
Ryo Yasuda <e165729@ie.u-ryukyu.ac.jp>
2016-11-22 18:25:39 +0900

    Report4 is compleated

--- src/main/jp/ac/uryukyu/ie/e165729/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e165729/LivingThing.java
@@ -21,7 +21,20 @@ public class LivingThing {
         return dead;
     }
 
+        public void setDead(boolean dead){this.dead = dead;} 
 
+public String getName() {  return name; }
+     
+public void setName(String name){this.name = name;} 
+
+     
+    public  int  getHitPoint(){ return  hitPoint;} 
+
+    public void setHitPoint(int hitPoint){this.hitPoint = hitPoint;}
+     
+public  int  getAttack(){ return attack;} 
+
+public void setAttack(int attack){this.attack = attack;}
 
     public void attack(LivingThing opponent) {
         if (dead == false) {
